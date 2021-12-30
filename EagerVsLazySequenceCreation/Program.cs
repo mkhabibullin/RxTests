7ffe1387ec11f77a6e716153a87ebf13093abe97
Marat Khabibullin <marat_khabibullin@epam.com>
2021-12-30 16:12:46 +0300

    +

---
+++ EagerVsLazySequenceCreation/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading;
+
+namespace EagerVsLazySequenceCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eagerly created sequence");
+            var eagerSequence = BlockingMethod();
+            var subscription = eagerSequence.Subscribe(Console.WriteLine);
+            subscription.Dispose();
+
+            Console.WriteLine("Press key");
+            Console.ReadKey();
+
+            Console.WriteLine("Lazierly created sequence");
+            var lazySequence = NonBlocking();
+            subscription = lazySequence.Subscribe(Console.WriteLine);
+            subscription.Dispose();
+
+            Console.WriteLine("Press key to exit");
+            Console.ReadKey();
+        }
+
+        private static IObservable<string> BlockingMethod()
+        {
+            var subject = new ReplaySubject<string>();
+            subject.OnNext("a");
+            subject.OnNext("b");
+            subject.OnCompleted();
+            Thread.Sleep(5000); // blocks subscription until 5 sec pass
+            return subject;
+        }
+        private static IObservable<string> NonBlocking()
+        {
+            return Observable.Create<string>(
+            (IObserver<string> observer) =>
+            {
+                observer.OnNext("a");
+                observer.OnNext("b");
+                observer.OnCompleted();
+                Thread.Sleep(5000); // Doesn't block subscription
+                return Disposable.Create(() => Console.WriteLine("Observer has unsubscribed"));
+                //or can return an Action like 
+                //return () => Console.WriteLine("Observer has unsubscribed"); 
+            });
+        }
+    }
+}
---
+++ HotObservable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace HotObservable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var period = TimeSpan.FromSeconds(1);
+            var observable = Observable.Interval(period)
+                .Do(l => Console.WriteLine("Publishing {0}", l)) //Side effect to show it is running
+                .Publish();
+
+            observable.Connect();
+
+            Console.WriteLine("Press any key to subscribe");
+            Console.ReadKey();
+
+            var subscription = observable.Subscribe(i => Console.WriteLine("subscription : {0}", i));
+
+            Console.WriteLine("Press any key to unsubscribe.");
+            Console.ReadKey();
+
+            subscription.Dispose();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
